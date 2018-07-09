22516283b7dd2ff2c213a66a840cfb8988238b70
xupeng <1219932032@qq.com>
2018-07-09 17:44:58 +0800

    test

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var watch = require("gulp-watch");
+var px2rem = require('gulp-px2rem-plugin');
+gulp.task("sass", function () {
+    var data = gulp.src(["./scss/*.scss"])
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(px2rem({'width_design':750,'valid_num':4,'pieces':10,'ignore_px':[1,2]}))
+        .pipe(gulp.dest("./css"));
+    return data
+});
+
+gulp.task("watch", function () {
+    gulp.watch(["./scss/*.scss"], ["sass"]);
+});
