1e09655ec95007841f606156253deeeaceb19fad
xupeng <1219932032@qq.com>
2018-07-10 18:12:39 +0800

    t

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var px2rem = require('gulp-px2rem-plugin');
 gulp.task("sass", function () {
     var data = gulp.src(["./scss/*.scss"])
         .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(px2rem({'width_design':750,'valid_num':4,'pieces':10,'ignore_px':[1,2]}))
+        .pipe(px2rem({'width_design':750,'valid_num':4,'pieces':10,'ignore_px':[1,2,-1,-2]}))
         .pipe(gulp.dest("./css"));
     return data
 });
